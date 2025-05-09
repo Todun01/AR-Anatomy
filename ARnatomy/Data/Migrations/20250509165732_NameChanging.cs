5641e34b9e9406659d102b08216fa3c8f88ed098
Oluwatodunni <obafemitodunni@gmail.com>
2025-05-09 17:59:05 +0100

    migrations added and database updated

---
+++ ARnatomy/Data/Migrations/20250509165732_NameChanging.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ARnatomy.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class NameChanging : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Feedback_OrganModels_OrganModelsId",
+                table: "Feedback");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Feedback_OrganModelsId",
+                table: "Feedback");
+
+            migrationBuilder.DropColumn(
+                name: "OrganModelsId",
+                table: "Feedback");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Feedback_OrganModelId",
+                table: "Feedback",
+                column: "OrganModelId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Feedback_OrganModels_OrganModelId",
+                table: "Feedback",
+                column: "OrganModelId",
+                principalTable: "OrganModels",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Feedback_OrganModels_OrganModelId",
+                table: "Feedback");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Feedback_OrganModelId",
+                table: "Feedback");
+
+            migrationBuilder.AddColumn<int>(
+                name: "OrganModelsId",
+                table: "Feedback",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Feedback_OrganModelsId",
+                table: "Feedback",
+                column: "OrganModelsId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Feedback_OrganModels_OrganModelsId",
+                table: "Feedback",
+                column: "OrganModelsId",
+                principalTable: "OrganModels",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
