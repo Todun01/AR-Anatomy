dcbeb578a95b85dd74d11c284c920406ccd6049c
Oluwatodunni <obafemitodunni@gmail.com>
2025-05-09 18:02:44 +0100

    database updated

---
+++ ARnatomy/Data/Migrations/20250509170138_DataTypeCorrection.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ARnatomy.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class DataTypeCorrection : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Rating",
+                table: "Feedback",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "Rating",
+                table: "Feedback",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+    }
+}
