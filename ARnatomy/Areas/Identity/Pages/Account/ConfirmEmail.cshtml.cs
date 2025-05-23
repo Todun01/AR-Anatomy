20e1ab5bcfd4e3ea1e85efe5efc4ba1faf19805f
Oluwatodunni <obafemitodunni@gmail.com>
2025-05-23 17:24:17 +0100

    Update ConfirmEmail.cshtml.cs

--- ARnatomy/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
+++ ARnatomy/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
@@ -83,7 +83,7 @@ namespace ARnatomy.Areas.Identity.Pages.Account
 
             code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
             var result = await _userManager.ConfirmEmailAsync(user, code);
-            StatusMessage = result.Succeeded ? "Thank you for confirming your email." : "Error confirming your email.";
+            StatusMessage = result.Succeeded ? "Congrats! Your email has been confirmed" : "Error confirming your email.";
             return Page(); // return
         }
     }
