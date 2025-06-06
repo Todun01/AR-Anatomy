1750396bf2551f640b056fe327ebb18ea4ff0975
Oluwatodunni <obafemitodunni@gmail.com>
2025-06-06 19:50:34 +0100

    implementing condition for if  device is ios or not

--- ARnatomy/wwwroot/js/site.js
+++ ARnatomy/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function isIos() {
+    const ua = window.navigator.userAgent;
+    const iOS = /iPad|iPhone|iPod/.test(ua);
+    const iPadOS = navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1;
+
+    return iOS || iPadOS;
+}
