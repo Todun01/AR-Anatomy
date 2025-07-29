57f6e621636e6086354365a6aa5fa308514ec8a9
Oluwatodunni <obafemitodunni@gmail.com>
2025-07-29 19:03:06 +0100

    updates

--- ARnatomy/wwwroot/js/site.js
+++ ARnatomy/wwwroot/js/site.js
@@ -13,9 +13,8 @@ function isIos() {
 document.addEventListener("DOMContentLoaded", function () {
     if (isIos()) {
         // full skeletal model
-        document.getElementById("view-in-ar-full").rel = "ar";
-        document.getElementById("view-in-ar-full").href = ""
+        document.getElementById("skeletal-full").href = "https://ar-natomy.b-cdn.net/USDZ/skeletal-full.usdz";
     } else {
-        document.getElementById("view-in-ar-full").href = "/AFRAME-Scenes/skeletal-full.html";
+        document.getElementById("skeletal-full").href = "intent://arvr.google.com/scene-viewer/1.0?file=https://ar-natomy.b-cdn.net/GLB/skeletal-full.glb&mode=ar_preferred#Intent;scheme=https;package=com.google.ar.core;action=android.intent.action.VIEW;S.browser_fallback_url=https://ar-natomy.tau.edu.ng;end;";
     }
 });
